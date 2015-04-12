a5e3f35e42b10be8eb2821d203c3c24ba3cd40bb
Jachu <jazuma5@gmail.com>
2015-04-12 22:52:51 +0200

    Unit test for the UpperCase test

--- MapExample/tests/MapJava8Test.java
+++ MapExample/tests/MapJava8Test.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,10 +9,17 @@ import static org.junit.Assert.*;
 public class MapJava8Test {
 
     Integer[] numbers = new Integer[]{1,2,10,5,6};
+    String[] letters = new String[]{"a","b","c","ab","cb"};
 
 
     @org.junit.Test
     public void testPowerTest() throws Exception {
         Assert.assertEquals(Arrays.asList(1,4,100,25,36),MapJava8.powerTest(numbers));
     }
+
+    @Test
+    public void testUpperCaseTest() throws Exception {
+        Assert.assertEquals(Arrays.asList("A","B","C","AB","CB"),MapJava8.toUpperCase(Arrays.asList(letters)));
+    }
 }
+
